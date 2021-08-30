febdfc5733cb02c2fce9f1fddeeec05358ffd0af
Leies <lei@lei202.com>
2021-08-30 21:16:21 +0900

    Update update.sh

--- update/update.sh
+++ update/update.sh
@@ -1,15 +1,26 @@
 #!/bin/bash
+echo -n "マージちゃんとやった？ [Y/n]: "
+read ANS
 
+case $ANS in
+  "" | [Yy]* )
+
+echo "アップデートSTART!!"
 cd /home/misskey/misskey
 
 git pull
 
 php update.php
 
-yarn install
+NODE_ENV=production yarn install --force
 NODE_ENV=production yarn build
 rm -rf /tmp/nginx_cache
 nginx -s reload
 php reboot.php
 sleep 10s
 systemctl restart misskey
+    ;;
+  * )
+    echo "んじゃマージしてもろて"
+    ;;
+esac
