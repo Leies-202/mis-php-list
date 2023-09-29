b3242c2274301283aaf69cee136b5bd95c7f5fde
Leies <lei@lei202.com>
2023-09-29 23:06:38 +0900

    Update update.sh
    コンフリ解消全くわからないので強行突破

--- update/update.sh
+++ update/update.sh
@@ -8,12 +8,15 @@ case $ANS in
 echo "アップデートSTART!!"
 cd /home/misskey/misskey
 
+rm -r -f /home/misskey/misskey/package.json
+rm -r -f /home/misskey/misskey/pnpm-lock.yaml
+
 git pull
 
 php update.php
 
-NODE_ENV=production yarn install --force
-NODE_ENV=production yarn build
+NODE_ENV=production pnpm i
+NODE_ENV=production pnpm build
 rm -rf /tmp/nginx_cache
 nginx -s reload
 php reboot.php
@@ -21,6 +24,7 @@ sleep 10s
 systemctl restart misskey
     ;;
   * )
+    # ここに「No」の時の処理を書く
     echo "んじゃマージしてもろて"
     ;;
 esac
